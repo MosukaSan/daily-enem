58536aaedae31c12e71dca43466abb2bd303e341
Lucas Lima <mosukasangamer@gmail.com>
2025-08-08 13:19:16 -0300

    decrease footer font size

--- src/views/components/Footer.tsx
+++ src/views/components/Footer.tsx
@@ -6,13 +6,13 @@ import type { JSX } from "react";
 function Footer(): JSX.Element {
     return (
         <footer className="border-t-3 border-dark-highlight">
-            <div className="container mx-auto flex justify-between items-center py-7 px-3 text-text text-xl">
+            <div className="container mx-auto flex justify-between items-center py-5 px-3 text-text">
                 <div>
                     <a href="https://github.com/MosukaSan" className="hover:text-text-hover"><FontAwesomeIcon icon={faGithub} /> Desenvolvedor</a>
                 </div>
-                <div onClick={() => window.location.href = "https://enem.dev/"} className="flex gap-3 items-center hover:cursor-pointer">
-                    <h3 className="hover:text-text-hover">Feito com a API Enem</h3>
-                    <img src={apiEnemIcon} alt="api enem image" className="h-10" />
+                <div onClick={() => window.location.href = "https://enem.dev/"} className="flex gap-3 items-center hover:cursor-pointer hover:text-text-hover">
+                    <h3>Feito com a API Enem</h3>
+                    <img src={apiEnemIcon} alt="api enem image" className="h-9" />
                 </div>
             </div>
         </footer>
